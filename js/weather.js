a593218a8b76f08e1898f381d6d9a1148e402606
DKL <95444292+DKL1231@users.noreply.github.com>
2022-07-16 19:15:17 +0900

    create weather,js
    end of lecture

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+const API_KEY = "e1a2944edaf09c7ff1cfb1dd95a7f057";
+const LANG = "en";
+
+function onGeoSuccess(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=${LANG}&units=metric`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
